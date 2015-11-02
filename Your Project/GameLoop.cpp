eb04889d0eddef3077d2ec3dcd3ca5d32ea9450b
LoganPletcher <lpletch95@hotmail.com>
2015-11-02 17:57:12 -0600

    1st Commit

--- Your Project/GameLoop.cpp
+++ Your Project/GameLoop.cpp
@@ -1,6 +1,21 @@
+#include <thread>
+#include <chrono>
 #include "GameLoop.h"
 
+void BULLET(int &bullet)
+{
+	bullet -= 10;
+}
 
+int x = 800;
+int y = 450;
+int size = 200;
+int sides = 50;
+int R = 0;
+int G = 255;
+int B = 255;
+int density = 150;
+int bullet = 1610;
 
 void GameLoop::Loop()
 {
@@ -25,6 +40,26 @@ void GameLoop::Loop()
 			LateUpdate();
 
 			Draw();
+
+			for (int i = 0; i < 2; i++)
+			{
+				if (bullet = -10)
+				{
+					bullet = 1610;
+					break;
+				}
+				else if (bullet < 1610)
+				{
+					this_thread::sleep_for(chrono::seconds(1));
+					BULLET(bullet);
+					Draw();
+					i = 0;
+				}
+				else
+				{
+					break;
+				}
+			}
 
 			Graphics::Flip(); // Required to update the window with all the newly drawn content
 		}
@@ -52,14 +87,36 @@ void GameLoop::Draw()
 	Graphics::DrawPoint({ 5, 5 }, { 255, 255, 255, 255 });
 
 	Graphics::DrawRing({ 140, 140 }, 50, 25, { 50, 0, 200, 255 });
-	Graphics::DrawCircle({ 800, 450 }, 200, 50, { 0, 255, 255, 150 });
+	Graphics::DrawCircle({ x, y }, size, sides, { R, G, B, density });
+
+	Graphics::DrawRing({ 140, 140 }, 50, 25, { 50, 0, 200, 255 });
+	Graphics::DrawCircle({ bullet, 450 }, 10, 500, { 200,200,200,255 });
+
 }
 
 void GameLoop::OnKeyDown(const SDL_Keycode ac_sdlSym, const Uint16 ac_uiMod, const SDL_Scancode ac_sdlScancode)
 {
-	
+	printf("%s\n", SDL_GetKeyName(ac_sdlSym));
 	switch (ac_sdlSym)
 	{
+	case SDLK_b: BULLET(bullet); break;
+	case SDLK_w: y -= 10; break;
+	case SDLK_s: y += 10; break;
+	case SDLK_d: x += 10; break;
+	case SDLK_a: x -= 10; break;
+	case SDLK_q: if (size > 0) { size -= 1; break; } else { break; }
+	case SDLK_e: size += 1; break;
+	case SDLK_r: sides += 1; break;
+	case SDLK_f: if (sides > 3) { sides -= 1; break; } else { break; }
+	case SDLK_y: if (R < 255) { R += 1; break; } else { break; }
+	case SDLK_h: if (R > 0) { R -= 1;  break; } else { break; }
+	case SDLK_u: if (G < 255) { G += 1; break; } else { break; }
+	case SDLK_j: if (G > 0) { G -= 1;  break; } else { break; }
+	case SDLK_i: if (B < 255) { B += 1; break; } else { break; }
+	case SDLK_k: if (B > 0) { B -= 1; break; } else { break; }
+	case SDLK_o: if (density < 255) { density += 1; break; } else { break; }
+	case SDLK_l: if (density > 0) { density -= 1; break; } else { break; }
+
 	case SDLK_ESCAPE: m_bRunning = false; break; // End the loop
 
 	default: printf("%s\n",SDL_GetKeyName(ac_sdlSym)); break;
@@ -69,6 +126,7 @@ void GameLoop::OnKeyUp(const SDL_Keycode ac_sdlSym, const Uint16 ac_uiMod, const
 {
 	switch (ac_sdlSym)
 	{
+
 	default: break;
 	}
 }
